83c78e58fd24492da856a67f8a63418bcc047d6c
Leonardo Jose <ljcl79@gmail.com>
2023-01-22 21:30:37 -0300

    Aula2

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,32 +1,27 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-var nombreArchivo = "Cuentas.txt";
-int numeroBytesLeidos = -1;
-var flujoArchivo = new FileStream(nombreArchivo, FileMode.Open);
-var buffer = new byte[1024];
-
-while(numeroBytesLeidos != 0)
+partial class Program
 {
-    numeroBytesLeidos = flujoArchivo.Read(buffer, 0, 1024);
-    escribirBuffer(buffer);
-}
-
-
-
-//public override int Read(byte[] buffer, int offset, int count);
-Console.WriteLine("Finalizada la lectura inicial");
-Console.ReadLine();
-
-void escribirBuffer(byte[] buffer)
-{
-    var utf8 = new UTF8Encoding();
-
-    Console.Write(utf8.GetString(buffer));
-    /*foreach(var cadaByte in buffer)
+    static void Main(string[] args)
     {
-        Console.Write(cadaByte);
-        Console.Write(" ");
-    }*/
-    return;
+        var nombreArchivo = "Cuentas.txt";
+
+        using (var flujoArchivo = new FileStream(nombreArchivo, FileMode.Open))
+        {
+            var lector = new StreamReader(flujoArchivo);
+
+            //var linea = lector.ReadLine();
+            //var contenido = lector.ReadToEnd();
+            //var caracter = lector.Read();
+
+            while(!lector.EndOfStream)
+            {
+                var linea = lector.ReadLine();
+                Console.WriteLine(linea);
+            }
+                        
+        }
+        Console.ReadLine();
+    }
 }
---
+++ ByteBankIO/TrabajandoConFileStream.cs
@@ -0,0 +1,43 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void TrabajandoConFileStream()
+    {
+        var nombreArchivo = "Cuentas.txt";
+
+        using (var flujoArchivo = new FileStream(nombreArchivo, FileMode.Open))
+        {
+            int numeroBytesLeidos = -1;
+            var buffer = new byte[1024];
+
+            while (numeroBytesLeidos != 0)
+            {
+                numeroBytesLeidos = flujoArchivo.Read(buffer, 0, 1024);
+                escribirBuffer(buffer, numeroBytesLeidos);
+            }
+
+        }
+
+
+
+
+        //public override int Read(byte[] buffer, int offset, int count);
+        Console.WriteLine("\nFinalizada la lectura inicial");
+        Console.ReadLine();
+
+        static void escribirBuffer(byte[] buffer, int numeroBytesLeidos)
+        {
+            var utf8 = new UTF8Encoding();
+
+            Console.Write(utf8.GetString(buffer, 0, numeroBytesLeidos));
+            /*foreach(var cadaByte in buffer)
+            {
+                Console.Write(cadaByte);
+                Console.Write(" ");
+            }*/
+            return;
+        }
+    }
+}
